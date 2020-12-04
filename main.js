9d210cfa64da7966b6332898bb9080a401b81c95
Anastasiia Gvozdieva <nastiagvozdieva@gmail.com>
2020-12-04 22:54:08 +0200

    slider complited

--- main.js
+++ main.js
@@ -1,12 +1,88 @@
-let position = 0;
-const slideToShow = 3;
-const slideToScroll = 1;
+'use strict';
+var multiItemSlider = (function () {
+  return function (selector, config) {
+    var
+      _mainElement = document.querySelector(selector), // основный элемент блока
+      _sliderWrapper = _mainElement.querySelector('.slider__wrapper'), // обертка для .slider-item
+      _sliderItems = _mainElement.querySelectorAll('.slider__item'), // элементы (.slider-item)
+      _sliderControls = _mainElement.querySelectorAll('.slider__control'), // элементы управления
+      _sliderControlLeft = _mainElement.querySelector('.slider__control_left'), // кнопка "LEFT"
+      _sliderControlRight = _mainElement.querySelector('.slider__control_right'), // кнопка "RIGHT"
+      _wrapperWidth = parseFloat(getComputedStyle(_sliderWrapper).width), // ширина обёртки
+      _itemWidth = parseFloat(getComputedStyle(_sliderItems[0]).width), // ширина одного элемента    
+      _positionLeftItem = 0, // позиция левого активного элемента
+      _transform = 0, // значение транфсофрмации .slider_wrapper
+      _step = _itemWidth / _wrapperWidth * 100, // величина шага (для трансформации)
+      _items = []; // массив элементов
+    // наполнение массива _items
+    _sliderItems.forEach(function (item, index) {
+      _items.push({ item: item, position: index, transform: 0 });
+    });
 
-const conteiner = document.querySelector('.slider-conteiner');
-const track = document.querySelector('.slider-track');
-const btnPrev = document.querySelector('.btnPrev');
-const btnNext = document.querySelector('.btnNext');
+    var position = {
+      getMin: 0,
+      getMax: _items.length - 1,
+    }
 
-const itemsCount = document.querySelectorAll('.sliderItem').length;
-const itemWidth = conteiner.clientWidth / slidesToShow;
-const movePosition = slideToScroll * itemWidth;
+    var _transformItem = function (direction) {
+      if (direction === 'right') {
+        if ((_positionLeftItem + _wrapperWidth / _itemWidth - 1) >= position.getMax) {
+          return;
+        }
+        if (!_sliderControlLeft.classList.contains('slider__control_show')) {
+          _sliderControlLeft.classList.add('slider__control_show');
+        }
+        if (_sliderControlRight.classList.contains('slider__control_show') && (_positionLeftItem + _wrapperWidth / _itemWidth) >= position.getMax) {
+          _sliderControlRight.classList.remove('slider__control_show');
+        }
+        _positionLeftItem++;
+        _transform -= _step;
+      }
+      if (direction === 'left') {
+        if (_positionLeftItem <= position.getMin) {
+          return;
+        }
+        if (!_sliderControlRight.classList.contains('slider__control_show')) {
+          _sliderControlRight.classList.add('slider__control_show');
+        }
+        if (_sliderControlLeft.classList.contains('slider__control_show') && _positionLeftItem - 1 <= position.getMin) {
+          _sliderControlLeft.classList.remove('slider__control_show');
+        }
+        _positionLeftItem--;
+        _transform += _step;
+      }
+      _sliderWrapper.style.transform = 'translateX(' + _transform + '%)';
+    }
+
+    // обработчик события click для кнопок "назад" и "вперед"
+    var _controlClick = function (e) {
+      if (e.target.classList.contains('slider__control')) {
+        e.preventDefault();
+        var direction = e.target.classList.contains('slider__control_right') ? 'right' : 'left';
+        _transformItem(direction);
+      }
+    };
+
+    var _setUpListeners = function () {
+      // добавление к кнопкам "назад" и "вперед" обрботчика _controlClick для событя click
+      _sliderControls.forEach(function (item) {
+        item.addEventListener('click', _controlClick);
+      });
+    }
+
+    // инициализация
+    _setUpListeners();
+
+    return {
+      right: function () { // метод right
+        _transformItem('right');
+      },
+      left: function () { // метод left
+        _transformItem('left');
+      }
+    }
+
+  }
+}());
+
+var slider = multiItemSlider('.slider')
